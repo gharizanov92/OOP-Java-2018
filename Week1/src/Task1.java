2ee6c52e246dcbfd22635a2cbf9d89f24d34bdd3
unknown <gharizanov92@gmail.com>
2018-10-13 15:13:24 +0300

    Week1

---
+++ Week1/src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+        double F = 55;
+        double C;
+
+        C = 5.0 / 9.0 * ( F - 32 );
+
+        System.out.printf("%.2f in Fahrenheit is %.2f in Celsius\n", F, C);
+    }
+}
